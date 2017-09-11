b91d3382dd22517e32f2dbe28acd77e4963c0429
Josh Barber <j.barber501@gmail.com>
2017-09-11 13:18:22 -0400

    Demo URL variable handling

--- hello_app.py
+++ hello_app.py
@@ -5,11 +5,24 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+	return "Index Page"
+
+@app_route('user/<username>')
+def show_user_profile(username):
+	#show the user profile for a given user - taken from entered URL
+	return "User %s" username
+
+@app.route('/post/<int:post_id>') 
+def show_post(post_id):
+	#show the post with the given id; the id must be an integer
+	return "Post %s" post_id
+
+@app.route('/hello')
 def hello_world():
-	pdb.set_trace() #Python debugger step-through
-	i = 3
-	visited = i + 1
-	return 'You have visited ' + str(visited) + ' times'
+	#Display hello world
+	#pdb.set_trace() #Python debugger step-through
+	return 'Hello World!'
 
 if __name__ == '__main__':
 	#GetEnv variables used as docker container does not do well with Flask defaults
